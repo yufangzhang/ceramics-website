4ae2708fff2589b49c88e2149e1fd149cf2fb776
Yufang Zhang <yufang_zhang@apple.com>
2026-02-11 01:48:02 +0000

    update to use domain as base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import sentry from '@sentry/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yufangzhang.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/ceramics-website' : '/',
+  site: 'https://yufangzhang.com',
+  base: '/',
   integrations: [
     tailwind(),
     sentry({
